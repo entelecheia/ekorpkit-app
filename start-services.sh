db5df5e31ff59466dd2dbfcaf14facd7e8c314bd
Young Joon Lee <yj.lee@chu.ac.kr>
2022-08-25 00:03:43 +0000

    udpated start-service not to detach

--- start-services.sh
+++ start-services.sh
@@ -5,6 +5,4 @@ source backend/.env
 source frontend/.env
 set +o allexport
 
-docker-compose up -d --remove-orphans
-  # --env-file backend/.env \
-  # --env-file frontend/.env \
+docker-compose up --remove-orphans
