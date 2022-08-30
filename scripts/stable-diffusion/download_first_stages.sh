bc4576b5ec82e683f5c5c2dcada2d890691238ea
entelecheia <yj.lee@chu.ac.kr>
2022-08-30 08:05:14 +0000

    updated download_models scripts for stable diffusion

--- scripts/stable-diffusion/download_first_stages.sh
+++ scripts/stable-diffusion/download_first_stages.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-wget --directory-prefix=models/first_stage_models/kl-f4/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f4.zip
-wget --directory-prefix=models/first_stage_models/kl-f8/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f8.zip
-wget --directory-prefix=models/first_stage_models/kl-f16/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f16.zip
-wget --directory-prefix=models/first_stage_models/kl-f32/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f32.zip
-wget --directory-prefix=models/first_stage_models/vq-f4/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f4.zip
-wget --directory-prefix=models/first_stage_models/vq-f4-noattn/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f4-noattn.zip
-wget --directory-prefix=models/first_stage_models/vq-f8/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f8.zip
-wget --directory-prefix=models/first_stage_models/vq-f8-n256/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f8-n256.zip
-wget --directory-prefix=models/first_stage_models/vq-f16/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f16.zip
+wget --directory-prefix=models/first_stage_models/kl-f4 https://ommer-lab.com/files/latent-diffusion/kl-f4.zip
+wget --directory-prefix=models/first_stage_models/kl-f8 https://ommer-lab.com/files/latent-diffusion/kl-f8.zip
+wget --directory-prefix=models/first_stage_models/kl-f16 https://ommer-lab.com/files/latent-diffusion/kl-f16.zip
+wget --directory-prefix=models/first_stage_models/kl-f32 https://ommer-lab.com/files/latent-diffusion/kl-f32.zip
+wget --directory-prefix=models/first_stage_models/vq-f4 https://ommer-lab.com/files/latent-diffusion/vq-f4.zip
+wget --directory-prefix=models/first_stage_models/vq-f4-noattn https://ommer-lab.com/files/latent-diffusion/vq-f4-noattn.zip
+wget --directory-prefix=models/first_stage_models/vq-f8 https://ommer-lab.com/files/latent-diffusion/vq-f8.zip
+wget --directory-prefix=models/first_stage_models/vq-f8-n256 https://ommer-lab.com/files/latent-diffusion/vq-f8-n256.zip
+wget --directory-prefix=models/first_stage_models/vq-f16 https://ommer-lab.com/files/latent-diffusion/vq-f16.zip
 
 
 
 cd models/first_stage_models/kl-f4
-unzip -o model.zip
+unzip -o kl-f4.zip
 
 cd ../kl-f8
-unzip -o model.zip
+unzip -o kl-f8.zip
 
 cd ../kl-f16
-unzip -o model.zip
+unzip -o kl-f16.zip
 
 cd ../kl-f32
-unzip -o model.zip
+unzip -o kl-f32.zip
 
 cd ../vq-f4
-unzip -o model.zip
+unzip -o vq-f4.zip
 
 cd ../vq-f4-noattn
-unzip -o model.zip
+unzip -o vq-f4-noattn.zip
 
 cd ../vq-f8
-unzip -o model.zip
+unzip -o vq-f8.zip
 
 cd ../vq-f8-n256
-unzip -o model.zip
+unzip -o vq-f8-n256.zip
 
 cd ../vq-f16
-unzip -o model.zip
+unzip -o vq-f16.zip
 
 cd ../..
--- scripts/stable-diffusion/download_models.sh
+++ scripts/stable-diffusion/download_models.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
-wget --directory-prefix=models/ldm/celeba256/celeba-256.zip https://ommer-lab.com/files/latent-diffusion/celeba.zip
-wget --directory-prefix=models/ldm/ffhq256/ffhq-256.zip https://ommer-lab.com/files/latent-diffusion/ffhq.zip
-wget --directory-prefix=models/ldm/lsun_churches256/lsun_churches-256.zip https://ommer-lab.com/files/latent-diffusion/lsun_churches.zip
-wget --directory-prefix=models/ldm/lsun_beds256/lsun_beds-256.zip https://ommer-lab.com/files/latent-diffusion/lsun_bedrooms.zip
-wget --directory-prefix=models/ldm/text2img256/model.zip https://ommer-lab.com/files/latent-diffusion/text2img.zip
-wget --directory-prefix=models/ldm/cin256/model.zip https://ommer-lab.com/files/latent-diffusion/cin.zip
-wget --directory-prefix=models/ldm/semantic_synthesis512/model.zip https://ommer-lab.com/files/latent-diffusion/semantic_synthesis.zip
-wget --directory-prefix=models/ldm/semantic_synthesis256/model.zip https://ommer-lab.com/files/latent-diffusion/semantic_synthesis256.zip
-wget --directory-prefix=models/ldm/bsr_sr/model.zip https://ommer-lab.com/files/latent-diffusion/sr_bsr.zip
-wget --directory-prefix=models/ldm/layout2img-openimages256/model.zip https://ommer-lab.com/files/latent-diffusion/layout2img_model.zip
-wget --directory-prefix=models/ldm/inpainting_big/model.zip https://ommer-lab.com/files/latent-diffusion/inpainting_big.zip
+wget --directory-prefix=models/ldm/celeba256 https://ommer-lab.com/files/latent-diffusion/celeba.zip
+wget --directory-prefix=models/ldm/ffhq256 https://ommer-lab.com/files/latent-diffusion/ffhq.zip
+wget --directory-prefix=models/ldm/lsun_churches256 https://ommer-lab.com/files/latent-diffusion/lsun_churches.zip
+wget --directory-prefix=models/ldm/lsun_beds256 https://ommer-lab.com/files/latent-diffusion/lsun_bedrooms.zip
+wget --directory-prefix=models/ldm/text2img256 https://ommer-lab.com/files/latent-diffusion/text2img.zip
+wget --directory-prefix=models/ldm/cin256 https://ommer-lab.com/files/latent-diffusion/cin.zip
+wget --directory-prefix=models/ldm/semantic_synthesis512 https://ommer-lab.com/files/latent-diffusion/semantic_synthesis.zip
+wget --directory-prefix=models/ldm/semantic_synthesis256 https://ommer-lab.com/files/latent-diffusion/semantic_synthesis256.zip
+wget --directory-prefix=models/ldm/bsr_sr https://ommer-lab.com/files/latent-diffusion/sr_bsr.zip
+wget --directory-prefix=models/ldm/layout2img-openimages256 https://ommer-lab.com/files/latent-diffusion/layout2img_model.zip
+wget --directory-prefix=models/ldm/inpainting_big https://ommer-lab.com/files/latent-diffusion/inpainting_big.zip
 
 
 
 cd models/ldm/celeba256
-unzip -o celeba-256.zip
+unzip -o celeba.zip
 
 cd ../ffhq256
-unzip -o ffhq-256.zip
+unzip -o ffhq.zip
 
 cd ../lsun_churches256
-unzip -o lsun_churches-256.zip
+unzip -o lsun_churches.zip
 
 cd ../lsun_beds256
-unzip -o lsun_beds-256.zip
+unzip -o lsun_bedrooms.zip
 
 cd ../text2img256
-unzip -o model.zip
+unzip -o text2img.zip
 
 cd ../cin256
 unzip -o model.zip
 
 cd ../semantic_synthesis512
-unzip -o model.zip
+unzip -o semantic_synthesis.zip
 
 cd ../semantic_synthesis256
-unzip -o model.zip
+unzip -o sr_bsr.zip
 
 cd ../bsr_sr
 unzip -o model.zip
 
 cd ../layout2img-openimages256
-unzip -o model.zip
+unzip -o layout2img_model.zip
 
 cd ../inpainting_big
-unzip -o model.zip
+unzip -o inpainting_big.zip
 
 cd ../..
