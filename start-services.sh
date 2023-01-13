5227c5d2d419a3afda69faa593033aa0efb1b18d
Young Joon Lee <entelecheia@hotmail.com>
2023-01-13 18:25:59 +0900

    updated start-service script

--- start-services.sh
+++ start-services.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 BUILD_PATH="$PWD"
-export BUILD_PATH
 
-docker-compose --project-directory ./ --file .docker/docker-compose.yaml --env-file .docker/.env.docker up 
+docker-compose --project-directory "${BUILD_PATH}" --file .docker/docker-compose.yaml --env-file .docker/.env.docker up 
